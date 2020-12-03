1b2a6ea920ff8aa9b5dfa237e7f32d8be01aca3c
StaronC <christian.staron84@gmail.com>
2020-12-03 18:28:48 +0100

    implementation histogramme 2D avec donnees 3D

--- Projet_BERD.m
+++ Projet_BERD.m
@@ -1,9 +1,40 @@
 close all 
 clear 
 
-%% Representation histogramme 3D
+%% Representation histogramme 2D avec donnees 3D
 
 I = imread('peppers.jfif');
 
-I = double(I);
+%I = double(I);
 
+colormap gray;
+dim = size(I);
+histo = zeros(256, 256, 256);
+for i=1:dim(1)
+  for j=1:dim(2)
+    r = I(i, j, 1)+1;
+    g = I(i, j, 2)+1;
+    b = I(i, j, 3)+1;
+    histo(r, g, b) = histo(r, g, b) + 1;
+  end
+end
+r = floor(double(img(i, j, 1)) * 192 / 256) + 1;
+
+imhist(histo(r, g, b));
+histoRG = sum(histo, 3);
+histoRB = squeeze(sum(histo, 2));
+histoGB = squeeze(sum(histo, 1));
+subplot(2, 2, 1); 
+imshow(I);
+subplot(2, 2, 2); 
+image(histoRG);
+xlabel('g');
+ylabel('r');
+subplot(2, 2, 3); 
+image(histoRB);
+xlabel('b');
+ylabel('r');
+subplot(2, 2, 4); 
+image(histoGB);
+xlabel('b');
+ylabel('g');
